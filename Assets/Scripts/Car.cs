9181205266a9a308b81852a2bba31dce5ffb8358
Salikh Magomedov <salikh.magomedov@icloud.com>
2021-06-27 11:55:37 +0300

    Steering The Car

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -4,10 +4,19 @@ public class Car : MonoBehaviour
 {
     [SerializeField] private float speed = 10f;
     [SerializeField] private float speedGainPerSecond = .2f;
+    [SerializeField] private float turnSpeed = 200f;
+    
+    private int _steerValue;
     
     private void Update()
     {
         speed += speedGainPerSecond * Time.deltaTime;
+        transform.Rotate(0, _steerValue * turnSpeed * Time.deltaTime, 0);
         transform.Translate(Vector3.forward * (speed * Time.deltaTime));
+    }
+
+    public void Steer(int value)
+    {
+        _steerValue = value;
     }
 }
