f44cfae012454d0ad6c477ac64789e4665fb752d
Salikh Magomedov <salikh.magomedov@icloud.com>
2021-06-27 14:11:37 +0300

    Score System

---
+++ Assets/Scripts/ScoreSystem.cs
@@ -0,0 +1,16 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ScoreSystem : MonoBehaviour
+{
+    [SerializeField] private TMP_Text scoreText;
+    [SerializeField] private float scoreMultiplier = 1f;
+
+    private float _score;
+
+    private void Update()
+    {
+        _score += scoreMultiplier * Time.deltaTime;
+        scoreText.text = Mathf.FloorToInt(_score).ToString();
+    }
+}
